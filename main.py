fccf5702f8cee1a12e6a47178532fa5b9dc69e0d
Nicolas Martinez <ucgmv@student.kit.edu>
2022-01-10 16:30:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+x = pd.read_excel(r"C:\Users\User\Documents\ITBA\Karlsruhe\Introduction to ML\Project\ML Disposed.xlsx")
+print(x)
+
+

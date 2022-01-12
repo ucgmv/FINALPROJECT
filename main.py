40587e99665281cb84c85f08117c9805437a4553
Nicolas Martinez <ucgmv@student.kit.edu>
2022-01-12 10:15:01 +0100

    Reading Files

--- main.py
+++ main.py
@@ -4,8 +4,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import pandas as pd
-x = pd.read_excel(r"C:\Users\User\Documents\ITBA\Karlsruhe\Introduction to ML\Project\ML Disposed.xlsx")
-print(x)
+#import openpyxl as op
+a = pd.read_excel(r'2010+Census+Population+By+Zipcode+(ZCTA).xlsx')
+b = pd.read_excel(r"BACM Annex A.xlsx")
+c = pd.read_excel(r"BACM Disposed.xlsx")
+d = pd.read_excel(r"CS Annex A.xlsx")
+e = pd.read_excel(r"CS Disposed.xlsx")
+f = pd.read_excel(r"JPM Annex A(1).xlsx")
+g = pd.read_excel(r"JPM Disposed.xlsx")
+h = pd.read_excel(r"ML Annex A.xlsx")
+i = pd.read_excel(r"ML Disposed.xlsx")
+j = pd.read_excel(r"SBM Annex A.xlsx")
+k = pd.read_excel(r"SBM Disposed.xlsx")
+
+#Read excels.
+
+print(j)
+
+
+
 
 
 print("Hello")
